9070a2ef9b6ad88dd5c38cc89bdf5b9ecc7f1583
andy0532 <62333515+andy0532@users.noreply.github.com>
2026-04-17 15:43:03 +0000

    fix: 恢复竖屏时导航栏自动隐藏

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -519,7 +519,7 @@ export const createStyle = <T extends StyleSheet.NamedStyles<T>>(styles: T | Sty
 }
 
 export const isHorizontalMode = (width: number, height: number): boolean => {
-  return true // car-mode
+  return width / height > 1.2
 }
 
 
